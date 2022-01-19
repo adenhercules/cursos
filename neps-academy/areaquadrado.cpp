c93795c2ff1593083fdca1ab8928ecf84b3eb8c5
adenhercules <31899234+adenhercules@users.noreply.github.com>
2022-01-18 23:39:02 -0400

    Add files via upload

---
+++ neps-academy/areaquadrado.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<math.h>
+int main(){
+	int l;
+	int area;
+	scanf("%d",&l);
+	area=pow(l,2);
+	printf("%d",area);
+}
---
+++ neps-academy/binoecino.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int b,c,s;
+	scanf("%d%d",&b,&c);
+	s=b+c;
+	if(s%2==0){
+		printf("Bino");
+	}
+	else{
+		printf("Cino");
+	}
+}
